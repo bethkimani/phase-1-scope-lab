1921911a3202338930332f2b224a93e1bd78f45f
Beth Kimani <kbwaceke@gmail.com>
2024-10-01 15:52:02 +0300

    Add Javascript Scope

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';  // Declaring a global variable without var/let/const makes it global
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';  // This is the correct value to assign
+}
+
+const leastFavoriteCustomer = 'Karen';  // Declare the constant
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'John';  // This will throw an error because constants can't be changed
+}
